d6d74b6ff0b626ff6b49bb8ac206f5b3d9e67275
asherlr23 <73479844+asherlr23@users.noreply.github.com>
2020-11-26 22:35:34 -0500

    Main program

---
+++ Program.cs
@@ -0,0 +1,30 @@
+Using system;
+  
+Namespace ComunityProgram
+{
+   Class Program
+   {
+       static void Main(string[] args)
+       {
+        //Extra Settings
+        Console.Title = "App PublicTest"
+        Console.ForegroundColor = Consolecolor.Cyan;
+        Console.WindowHeight = 40;
+        //code Goes here
+        
+        Console.WriteLine("This is a template.")
+        Console.WriteLine("Press any key to close........")
+        
+        
+        
+        
+        
+        
+        
+        // Close the program by pressing a key
+        Console.Readkey();
+        
+       }
+    }   
+}     
+     
