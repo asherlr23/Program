3aa5c7722fcf29376d4870b844d8d569dfc1df22
Asherlr <73479844+asherlr23@users.noreply.github.com>
2020-11-30 13:43:30 -0500

    Add files via upload

---
+++ bin/Debug/5.0/mainaplication.cs
@@ -0,0 +1,20 @@
+using system;
+{
+    Namespace app;
+    {
+
+        Class program;
+        {
+
+            static void main(string[] args);
+            {
+              //To run the program, go to {run>start_debuging>.NET_core_launch}
+              //Extra settings
+              Console.title = "Debug window";
+              Console.ForegroundColor = Consolecolor.cyan;
+              Console.WindowHeight = 40;
+              //main code
+            }
+        }
+    }
+}
