34e0ec295703369a534d76da5f6eeac915ae4ca1
Asherlr <73479844+asherlr23@users.noreply.github.com>
2020-12-17 20:44:00 -0500

    Update mainaplication.cs

--- bin/Debug/5.0/mainaplication.cs
+++ bin/Debug/5.0/mainaplication.cs
@@ -11,7 +11,7 @@ using system;
               //To run the program, go to {run>start_debuging>.NET_core_launch}
               //Extra settings
               Console.title = "Debug window";
-              Console.ForegroundColor = Consolecolor.cyan;
+              Console.ForegroundColor = Consolecolor.random;
               Console.WindowHeight = 40;
               //main code
             }
